0b25e8f3d6b2e5372ba2641271ea208b1bad7db5
Aayush <aayushmishra736@gmail.com>
2022-10-26 22:31:35 +0530

    initlal commit

---
+++ javascript/javascriptprogramming/biggestoftwo.js
@@ -0,0 +1,12 @@
+var num1 = 100;
+var num2 = 200;
+if(num1 > num2){
+    console.log("Bigger Number is :",num1);
+}
+else
+if(num2 > num1){
+console.log("Bigger Number is : ",num2)
+}
+else{
+    console.log("Numbers are equal.")
+}
---
+++ javascript/javascriptprogramming/findSign.js
@@ -0,0 +1,20 @@
+var i = 3;
+var j = -7;
+var k = 2;
+if(i < 0 || j < 0 || k < 0){
+    if(i < 0){
+        console.log("The sign for i is : - " );
+    }
+    else
+        if(j < 0){
+            console.log("The sign for j is  : - " );
+        }
+    else
+        if(k < 0){
+            console.log("The sign for k is  - " );
+        }    
+}
+else
+{
+    alert("All are positive number");
+}
