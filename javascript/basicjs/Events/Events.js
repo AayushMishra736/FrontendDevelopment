b653a7e5721b9ccb5b4a78ba8d4cc69d27faec9c
Aayush <aayushmishra736@gmail.com>
2022-11-12 09:44:10 +0530

    initial commit

--- javascript/basicjs/Events/Events.js
+++ javascript/basicjs/Events/Events.js
@@ -1,9 +1,13 @@
 function mOver(obj) {
     obj.innerHTML = "Thank You"
   }
-  function mOut(obj) {
-    obj.innerHTML = "Mouse Over Me"
+function mOut() {
+document.getElementById.innerHTML("mOut") = "Mouse is out after Out";
+    // obj.innerHTML = "Mouse Over Me"
   }
-function onclcikbutton(obj){
-    obj.innerHTML = 
+function onClickButton() {
+    const con = document.getElementById("btn");
+    con.style.backgroundColor = "#FFFFF";
 }
+
+
