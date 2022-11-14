2e03a14468d1ab91eea5357f547238383ddb8b30
Aayush <aayushmishra736@gmail.com>
2022-11-14 23:29:26 +0530

    initial commit

---
+++ javascript/Events/events.js
@@ -0,0 +1,18 @@
+function On_Change(){
+    document.getElementById("a").innerHTML.style.backgroundColor = "red";
+}
+function onclick_button(){
+    document.getElementById("b").style.backgroundColor = "red";
+}
+function onmouse_over(){
+    document.getElementById("c").style.backgroundColor = "red";
+}
+function onmouse_out(){
+    document.getElementById("d").style.backgroundColor = "red";
+}
+function onkey_down(){
+    document.getElementById("e").style.backgroundColor = "red";
+}
+function on_load(){
+    document.getElementById("f").style.backgroundColor = "red";
+}
